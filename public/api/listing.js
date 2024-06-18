ec927eb1d51071d89700592f4599d42558e427b0
Aryaman <aryamanyash11@gmail.com>
2024-06-18 23:43:08 +0530

    first commit

---
+++ public/api/listing.js
@@ -0,0 +1,122 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://aryamanyash11:Aryaman11@cluster0.pwvo6oj.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+}); 
+
+module.exports = async function(req,res){
+    // console.log(">>>>>>> query -----1",req.query);
+    let country = req.query.country;
+  try{
+    let data = []
+      if(Object.keys(req.query).length == 0){
+        console.log(">>>>>>> query -----1");
+         let data = await client.db("sample_airbnb").collection("listingsAndReviews").aggregate([
+    {
+      $match : {"minimum_nights":  "1"}
+    },
+          {
+      
+      $project: {
+        _id: 1,
+        name:1,
+        summary:1,
+        overall_rating:{
+               $avg: [
+                "$review_scores.review_scores_accuracy", 
+                "$review_scores.review_scores_cleanliness", 
+                "$review_scores.review_scores_checkin", 
+                "$review_scores.review_scores_communication", 
+                "$review_scores.review_scores_location", 
+                "$review_scores.review_scores_value"
+            ]
+            },
+        price:{
+                $convert:{
+                    input: "$price",
+                    to: "double",
+                    onError: 0.0,  // If conversion fails, return 0.0
+                    onNull: 0.0    // If field is null, return 0.0
+                }
+            },
+        tax: {
+                $convert: {
+                input: "$cleaning_fee",
+                to: "double",
+                onError: 0.0,  // If conversion fails, return 0.0
+                onNull: 0.0    // If field is null, return 0.0
+                }},
+        photo : "$images.picture_url",
+        review_count:"$number_of_reviews",
+      }
+    }
+       ]).limit(10).toArray()
+      
+        return{
+        status: "200",
+        message: "success",
+        response: data
+        }
+
+      }
+      else if(country){
+        let data = await client.db("sample_airbnb").collection("listingsAndReviews").aggregate([
+    {
+      $match: {
+        "address.country": country
+      }
+      },{
+      $project: {
+        _id: 1,
+        name:1,
+        summary:1,
+        overall_rating:{
+               $avg: [
+                "$review_scores.review_scores_accuracy", 
+                "$review_scores.review_scores_cleanliness", 
+                "$review_scores.review_scores_checkin", 
+                "$review_scores.review_scores_communication", 
+                "$review_scores.review_scores_location", 
+                "$review_scores.review_scores_value"
+            ]
+            },
+        price:{
+                $convert:{
+                    input: "$price",
+                    to: "double",
+                    onError: 0.0,  // If conversion fails, return 0.0
+                    onNull: 0.0    // If field is null, return 0.0
+                }
+            },
+        tax: {
+                $convert: {
+                input: "$cleaning_fee",
+                to: "double",
+                onError: 0.0,  // If conversion fails, return 0.0
+                onNull: 0.0    // If field is null, return 0.0
+                }},
+        photo : "$images.picture_url",
+        review_count:"$number_of_reviews",
+      }
+     }
+     ]).toArray()
+
+      return{
+      status: "200",
+      message: "success",
+      response: data
+      }
+
+      }
+
+
+
+ 
+  }catch(err){
+   console.log(">>>>>>> error",err)
+  }
+}
---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var home = require('../public/api/home');
+var detail = require('../public/api/detail');
+var checkout = require('../public/api/checkout');
+var listing = require('../public/api/listing');
+const app = express();
+const cors = require('cors'); 
+app.use(cors());
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/home', async (req, res) => {
+    const data = await home(req, res);  // Await the result of the async function
+    res.send(data);  // Send the result of the async function
+});
+
+router.get('/detail', async (req, res) => {
+    const data = await detail(req, res);  // Await the result of the async function
+    res.send(data);  // Send the result of the async function
+});
+
+router.get('/checkout', async (req, res) => {
+    const data = await checkout(req, res);  // Await the result of the async function
+    res.send(data);  // Send the result of the async function
+});
+
+router.get('/listing', async (req, res) => {
+    const data = await listing(req, res);  // Await the result of the async function
+    res.send(data);  // Send the result of the async function
+});
+
+module.exports = router;
